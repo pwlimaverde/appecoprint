d57bd8781be8f2447657fb2ec06673013e8b6d98
Paulo Weslley <vorfast@gmail.com>
2019-04-13 12:04:56 -0300

    inicio da implantação dos menus nav e remodelagem dos templats

--- baseof/settings.py
+++ baseof/settings.py
@@ -37,7 +37,7 @@ ROOT_URLCONF = 'baseof.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -86,3 +86,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
