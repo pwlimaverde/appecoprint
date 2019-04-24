d09fa8ed43a978860d73826cb869462d53652292
Paulo W. Limaverde <vorfast@gmail.com>
2019-04-24 01:14:16 -0300

    correção de bug migrate, exclusão de arquivos migrate antigos

--- modulopcp/models.py
+++ modulopcp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Cont_op(models.Model):
     op = models.CharField(max_length=200, blank=False, null=False)
     servico = models.CharField(max_length=200, blank=False, null=False)
+
+    def __str__(self):
+        return self.op + ' - ' + self.servico
