c7e08cf04df3c81e52a777aff7c98e1a0d72dc08
Paulo W. Limaverde <vorfast@gmail.com>
2019-04-28 16:12:28 -0300

    update xls até entrada

--- modulopcp/urls.py
+++ modulopcp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Testeup
+from .views import Testeup, Upload_op
 
 
 urlpatterns = [
-    path('', Testeup.as_view(), name='url_Testeimport'),
+    path('', Testeup.as_view(), name='url_testeimport'),
+    path('upload-op/', Upload_op.as_view(), name='url_upload_op'),
 ]
 
--- modulopcp/views.py
+++ modulopcp/views.py
@@ -2,8 +2,58 @@ from django.shortcuts import render
 from django.contrib import messages
 from django.views.generic.base import TemplateView, View
 from datetime import datetime
-from .models import Uploadxls, Testexls3
+from .models import Uploadxls, Testexls3, Ops, Upload_list_op
 import xlrd
+
+
+class Upload_op(View):
+
+    def get(self, request, *args, **kwargs):
+
+        prompt = {
+            'Upload do xml': 'Faça upload para inserir ou atualizar os dados'
+        }
+        return render(request, 'modulopcp/uploadop.html', prompt)
+
+    def post(self, request, *args, **kwargs):
+
+        xls_file = request.FILES['file']
+        Upload_list_op.objects.update_or_create(descricao=xls_file.name, arquivo=xls_file)
+
+        caminho = 'static/arquivosxls/'+xls_file.name
+
+        if not xls_file.name.endswith('.xls'):
+            messages.error(request, 'Não é um xml')
+
+        workbook = xlrd.open_workbook(caminho)
+        worksheet = workbook.sheet_by_index(0)
+        datemode = workbook.datemode
+
+        listaxls = []
+
+        for row_num in range(worksheet.nrows):
+            row = worksheet.row_values(row_num)
+            row[5] = datetime(*xlrd.xldate_as_tuple(row[5], datemode))
+            #row[8] = datetime(*xlrd.xldate_as_tuple(row[8], datemode))
+            listaxls.append(row)
+
+        for item in listaxls:
+            Ops.objects.update_or_create(
+                orcamento=item[0],
+                cliente=item[1],
+                servico=item[2],
+                quant=item[3],
+                valor=item[4],
+                entrada=item[5],
+            )
+
+        context = {}
+
+        context['listaxls'] = listaxls
+        context['now'] = datetime.now()
+
+        return render(request, 'modulopcp/uploadop.html', context)
+
 
 
 class Testeup(View):
@@ -20,7 +70,7 @@ class Testeup(View):
         xls_file = request.FILES['file']
         Uploadxls.objects.update_or_create(descricao=xls_file.name, arquivo=xls_file)
 
-        caminho = 'xls/' + xls_file.name
+        caminho = 'static/arquivosxls/'+xls_file.name
 
         if not xls_file.name.endswith('.xls'):
             messages.error(request, 'Não é um xml')
