d72f742d074f2c5b1ef0a6422d72f2697854b526
Paulo W. Limaverde <vorfast@gmail.com>
2019-04-28 12:25:50 -0300

    model ops funcionando

--- modulopcp/admin.py
+++ modulopcp/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Uploadxls, Testexls3, Upload_list_op
+from .models import Uploadxls, Testexls3, Upload_list_op, Ops
 
 
 admin.site.register(Uploadxls)
-admin.site.register(Upload_list_op)
 admin.site.register(Testexls3)
+admin.site.register(Upload_list_op)
+admin.site.register(Ops)
--- modulopcp/models.py
+++ modulopcp/models.py
@@ -18,3 +18,24 @@ class Upload_list_op(models.Model):
     descricao = models.CharField(max_length=200, blank=False, null=False)
     arquivo = models.FileField(max_length=200, upload_to='static/arquivosxls/', blank=False, null=False)
 
+
+class Ops(models.Model):
+    orcamento = models.IntegerField(max_length=5, blank=False, null=False)
+    cliente = models.CharField(max_length=300, blank=False, null=False)
+    servico = models.TextField(blank=False, null=False)
+    quant = models.DecimalField(max_digits=7, decimal_places=0, blank=False, null=False)
+    valor = models.DecimalField(max_digits=7, decimal_places=4, blank=False, null=False)
+    entrada = models.DateField(blank=False, null=False)
+    vendedor = models.CharField(max_length=100, blank=False, null=False)
+    op = models.IntegerField(max_length=5, blank=False, null=False)
+    prev_entrega = models.DateField(blank=False, null=False)
+    faturamento = models.CharField(max_length=100, blank=True, null=True)
+    entrega = models.DateField(blank=True, null=True)
+    cancelada = models.BooleanField(blank=True, null=True)
+    obs = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return str(self.op) + ' - ' + self.cliente
+
+
+
