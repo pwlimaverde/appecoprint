e13daf04b304b8b8d00dfb85dc4acd32528a90f4
Paulo W. Limaverde <vorfast@gmail.com>
2019-05-02 00:16:51 -0300

    correção bug deploy

--- modulopcp/models.py
+++ modulopcp/models.py
@@ -9,14 +9,14 @@ class Upload_list_opv2(models.Model):
         return self.descricao
 
 class Opsv2(models.Model):
-    orcamento = models.IntegerField(max_length=5, blank=False, null=False)
+    orcamento = models.IntegerField(blank=False, null=False)
     cliente = models.CharField(max_length=300, blank=False, null=False)
     servico = models.TextField(blank=False, null=False)
     quant = models.DecimalField(max_digits=7, decimal_places=0, blank=False, null=False)
     valor = models.DecimalField(max_digits=7, decimal_places=2, blank=False, null=False)
     entrada = models.DateField(blank=False, null=False)
     vendedor = models.CharField(max_length=100, blank=False, null=False)
-    op = models.IntegerField(max_length=5, blank=False, null=False)
+    op = models.IntegerField(blank=False, null=False)
     prev_entrega = models.DateTimeField(blank=False, null=False)
 
     def status(self):
